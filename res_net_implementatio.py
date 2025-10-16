815b92e85dccfe6ac08ff5c6625c4dea5503e428
kevinneung <94151024+kevinneung@users.noreply.github.com>
2025-10-16 16:20:12 -0700

    added jupyter notebook

--- res_net_implementatio.py
+++ res_net_implementatio.py
@@ -11,3 +11,39 @@ from keras.models import Model
 from keras.datasets import cifar10
 import numpy as np
 import os
+
+batch_size = 32
+epochs = 200
+data_augmentation = True
+num_classes = 10
+subtract_pixel_mean = True
+n = 3
+version = 1
+
+if version == 1:
+    depth = n * 6 + 2
+elif version == 2:
+    depth = n * 9 + 2
+model_type = 'ResNet % dv % d' % (depth, version)
+
+#Use the CIFAR-10 Dataset, it contains 60,000 color images of varying kinds across 10 classes
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+input_shape = x_train.shape[1:]
+
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+if subtract_pixel_mean:
+    x_train_mean = np.mean(x_train, axis = 0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
